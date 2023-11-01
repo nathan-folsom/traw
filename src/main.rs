d6764b6ac83841651d8617f0f43f43c5e2589b21
Nathan Folsom <n8folsom@gmail.com>
2023-10-31 23:48:31 -0700

    start building arrow mode, extract basic motion code

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use std::io::{stdout, Write};
 
+use arrow::Arrow;
 use crossterm::{
     cursor,
-    event::{self, KeyCode},
+    event::{self, KeyCode, KeyEvent},
     execute, queue,
     style::{Color, ResetColor, SetForegroundColor},
     terminal::{self, disable_raw_mode, enable_raw_mode},
@@ -10,6 +11,7 @@ use crossterm::{
 use draw::Renderer;
 use rectangle::Rectangle;
 
+mod arrow;
 mod draw;
 mod rectangle;
 
@@ -22,67 +24,72 @@ fn main() -> std::io::Result<()> {
 
     loop {
         match event::read()? {
-            event::Event::Key(key_event) => match key_event.code {
-                KeyCode::Char(key) => match &mut mode {
-                    Mode::Text(rect) => {
-                        rect.on_char(key);
-                        queue!(stdout(), cursor::MoveRight(1))?;
+            event::Event::Key(key_event) => {
+                match &mut mode {
+                    Mode::Normal => {
+                        handle_motions(key_event)?;
                     }
-                    Mode::Normal | Mode::Draw(_) => match key {
-                        'q' => break,
-                        'i' => match mode {
-                            Mode::Draw(rect) => {
-                                mode = Mode::Text(rect);
-                            }
-                            Mode::Normal => {
-                                let (x, y) = cursor::position()?;
-                                mode = Mode::Draw(Rectangle::new_at(x as i32, y as i32));
-                            }
+                    Mode::Text(_) => {}
+                    Mode::DrawRectangle(_) => {
+                        handle_motions(key_event)?;
+                    }
+                    Mode::DrawArrow(_) => {
+                        handle_motions(key_event)?;
+                    }
+                }
+                match key_event.code {
+                    KeyCode::Char(key) => match &mut mode {
+                        Mode::Text(rect) => {
+                            rect.on_char(key);
+                            queue!(stdout(), cursor::MoveRight(1))?;
+                        }
+                        Mode::Normal | Mode::DrawRectangle(_) => match key {
+                            'q' => break,
+                            'i' => match mode {
+                                Mode::DrawRectangle(rect) => {
+                                    mode = Mode::Text(rect);
+                                }
+                                Mode::Normal => {
+                                    let (x, y) = cursor::position()?;
+                                    mode =
+                                        Mode::DrawRectangle(Rectangle::new_at(x as i32, y as i32));
+                                }
+                                _ => {}
+                            },
                             _ => {}
                         },
-                        'h' => {
-                            execute!(stdout(), cursor::MoveLeft(1))?;
+                        _ => {}
+                    },
+                    KeyCode::Enter => match mode {
+                        Mode::DrawRectangle(rect) => {
+                            queue!(
+                                stdout(),
+                                cursor::MoveTo(rect.x as u16 + 1, rect.y as u16 + 1)
+                            )?;
+                            mode = Mode::Text(rect);
                         }
-                        'j' => {
-                            execute!(stdout(), cursor::MoveDown(1))?;
-                        }
-                        'k' => {
-                            execute!(stdout(), cursor::MoveUp(1))?;
-                        }
-                        'l' => {
-                            execute!(stdout(), cursor::MoveRight(1))?;
+                        Mode::Text(rect) => {
+                            rectangles.push(rect);
+                            mode = Mode::Normal;
                         }
                         _ => {}
                     },
-                },
-                KeyCode::Enter => match mode {
-                    Mode::Draw(rect) => {
-                        queue!(
-                            stdout(),
-                            cursor::MoveTo(rect.x as u16 + 1, rect.y as u16 + 1)
-                        )?;
-                        mode = Mode::Text(rect);
-                    }
-                    Mode::Text(rect) => {
-                        rectangles.push(rect);
-                        mode = Mode::Normal;
-                    }
                     _ => {}
-                },
-                _ => {}
-            },
+                }
+            }
             _ => {}
         }
 
         match &mut mode {
             Mode::Normal => {}
-            Mode::Draw(rect) => {
+            Mode::DrawRectangle(rect) => {
                 rect.update()?;
                 renderer.render(rect)?;
             }
             Mode::Text(rect) => {
                 renderer.render(rect)?;
             }
+            _ => {}
         }
 
         stdout().flush()?;
@@ -116,6 +123,30 @@ fn end() -> std::io::Result<()> {
 
 enum Mode {
     Normal,
-    Draw(Rectangle),
+    DrawRectangle(Rectangle),
+    DrawArrow(Arrow),
     Text(Rectangle),
+}
+
+fn handle_motions(event: KeyEvent) -> std::io::Result<()> {
+    match event.code {
+        KeyCode::Char(key) => match key {
+            'h' => {
+                execute!(stdout(), cursor::MoveLeft(1))?;
+            }
+            'j' => {
+                execute!(stdout(), cursor::MoveDown(1))?;
+            }
+            'k' => {
+                execute!(stdout(), cursor::MoveUp(1))?;
+            }
+            'l' => {
+                execute!(stdout(), cursor::MoveRight(1))?;
+            }
+            _ => {}
+        },
+        _ => {}
+    }
+
+    Ok(())
 }
